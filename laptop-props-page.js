a241de59ddbea16523c1362e1a2407c2629a778d
Luka <nozadzeluka2002@gmail.com>
2022-08-28 12:48:39 +0400

    fetch CPU API information and sort it

--- laptop-props-page.js
+++ laptop-props-page.js
@@ -1,4 +1,5 @@
 const brandSelectElement = document.getElementById("brand");
+const cpuSelectElement = document.getElementById("cpu");
 fetch("https://pcfy.redberryinternship.ge/api/brands")
   .then((r) => r.json())
   .then((res) => {
@@ -7,5 +8,16 @@ fetch("https://pcfy.redberryinternship.ge/api/brands")
       brandOptEl.value = item.name;
       brandOptEl.textContent = item.name;
       brandSelectElement.append(brandOptEl);
+    });
+  });
+
+fetch("https://pcfy.redberryinternship.ge/api/cpus")
+  .then((r) => r.json())
+  .then((res) => {
+    res.data.forEach((item) => {
+      const cpuOptEl = document.createElement("option");
+      cpuOptEl.value = item.name;
+      cpuOptEl.textContent = item.name;
+      cpuSelectElement.append(cpuOptEl);
     });
   });
