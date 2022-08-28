99fb8240b4e34182863050f4e30cb4007c464230
Luka <nozadzeluka2002@gmail.com>
2022-08-28 11:32:24 +0400

    fetch positions API information and sort it

--- workers-info-page.js
+++ workers-info-page.js
@@ -1,12 +1,23 @@
 const teamSelectElement = document.getElementById("team");
-
+const positionSelectElement = document.getElementById("position");
 fetch("https://pcfy.redberryinternship.ge/api/teams")
   .then((r) => r.json())
   .then((res) => {
     res.data.forEach((item) => {
-      const optEl = document.createElement("option");
-      optEl.value = item.name;
-      optEl.textContent = item.name;
-      teamSelectElement.append(optEl);
+      const teamOptEl = document.createElement("option");
+      teamOptEl.value = item.name;
+      teamOptEl.textContent = item.name;
+      teamSelectElement.append(teamOptEl);
+    });
+  });
+
+fetch("https://pcfy.redberryinternship.ge/api/positions")
+  .then((r) => r.json())
+  .then((res) => {
+    res.data.forEach((item) => {
+      const positionOptEl = document.createElement("option");
+      positionOptEl.value = item.name;
+      positionOptEl.textContent = item.name;
+      positionSelectElement.append(positionOptEl);
     });
   });
